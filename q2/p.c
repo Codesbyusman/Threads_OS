f1a010601e20ce0191cf30e36e4565704f37173d
codesbyusman <codesbyusman@gmail.com>
2022-05-05 12:05:09 +0500

    finding error -- ok till here

--- q2/p.c
+++ q2/p.c
@@ -7,16 +7,58 @@
 #include <fcntl.h>
 #include <string.h>
 
-int main()
+// for printing the matrices 
+void printMatrix(double array[][4], const int rows, const int cols)
 {
 
-    double a = 9.5;
+	// the upper
+	printf("\n\t  __");
 
-    printf("\n%f\n", a);
+	for (int i = 0; i <= cols; i++)
+	{
+		printf("\t ");
+	}
 
-    char c[5];
-    itoa(a, c, 10);
+	printf("\b\b\b __\n");
 
-    printf("\n%s\n", c);
-    return 0;
+	printf("\t |");
+
+	for (int i = 0; i <= cols; i++)
+	{
+		printf("\t ");
+	}
+
+	printf("\b\b  |\n");
+
+	// the main content
+	for (int i = 0; i < rows; i++)
+	{
+		printf("\t | ");
+		for (int j = 0; j < cols; j++)
+		{
+
+			printf("%8.2f", array[i][j]);
+		}
+
+		printf("\t | \n");
+	}
+
+	// the lower 
+	
+	printf("\t |_");
+
+	for (int i = 0; i <= cols; i++)
+	{
+		printf("\t ");
+	}
+
+	printf("\b\b _|\n\n");
+}
+int main(void)
+{
+
+	double number[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 11, 22};
+	printMatrix(number, 3, 4);
+
+	return 0;
 }
--- q2/q2.c
+++ q2/q2.c
@@ -26,6 +26,102 @@ double basisMatrix[weightMR][featureMC] = {1, 1, 1};                            
 double resultantMatrix[weightMR][featureMC];                                    // addition resultant
 double sigmoidMatrix[weightMR][featureMC];                                      // the sigmoid applieds
 
+// for printing the matrices
+void printMatrix(double array[][featureMC])
+{
+
+    // the upper
+    printf("\n\t  __");
+
+    for (int i = 0; i <= featureMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b\b __\n");
+
+    printf("\t |");
+
+    for (int i = 0; i <= featureMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b  |\n");
+
+    // the main content
+    for (int i = 0; i < weightMR; i++)
+    {
+        printf("\t | ");
+        for (int j = 0; j < featureMC; j++)
+        {
+
+            printf("%8.2f", array[i][j]);
+        }
+
+        printf("\t | \n");
+    }
+
+    // the lower
+
+    printf("\t |_");
+
+    for (int i = 0; i <= featureMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b _|\n\n");
+}
+
+// for printing the weight matrix
+void printWeightMatrix(double array[][weightMC])
+{
+
+    // the upper
+    printf("\n\t  __");
+
+    for (int i = 0; i <= weightMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b\b __\n");
+
+    printf("\t |");
+
+    for (int i = 0; i <= weightMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b  |\n");
+
+    // the main content
+    for (int i = 0; i < weightMR; i++)
+    {
+        printf("\t | ");
+        for (int j = 0; j < weightMC; j++)
+        {
+
+            printf("%8.2f", array[i][j]);
+        }
+
+        printf("\t | \n");
+    }
+
+    // the lower
+
+    printf("\t |_");
+
+    for (int i = 0; i <= weightMC; i++)
+    {
+        printf("\t ");
+    }
+
+    printf("\b\b _|\n\n");
+}
+
 int main()
 {
 
@@ -56,13 +152,21 @@ int main()
     // }
 
     // initalizing the basis matrix
-    for (int i = 0; i < weightMR; i++)
-    {
-        for (int j = 0; j < featureMC; j++)
-        {
-            basisMatrix[i][j] = rand() % 300 - 150.33;
-        }
-    }
+    // for (int i = 0; i < weightMR; i++)
+    // {
+    //     for (int j = 0; j < featureMC; j++)
+    //     {
+    //         basisMatrix[i][j] = rand() % 300 - 150.33;
+    //     }
+    // }
+
+    //   writing the result to the pipe --- mul results
+    int fdMulresult[2];
+    pipe(fdMulresult);
+
+    // writing result of addition
+    int fdAddresult[2];
+    pipe(fdAddresult);
 
     // the id's off functions
     pid_t prcssId1, prcssId2, prcssId3;
@@ -72,6 +176,8 @@ int main()
 
     if (prcssId1 == 0)
     {
+         printf("\n child 1 \n");
+
         // child
         // will do now multi threading --- such that
         // thredas created wll be equal to the number of the rows of weight matrix thus making a array for the thrread id's
@@ -97,9 +203,22 @@ int main()
         }
 
         // writing this matix output on pipe and will read from that
-        char Result[500];
-        char convert[5];
+        char result[500] = " ";
+        char convert[10];
 
+        for (int i = 0; i < weightMR; i++)
+        {
+            for (int j = 0; j < featureMC; j++)
+            {
+                sprintf(convert, "%.2f", mullResult[i][j]); // converting to string so to concatenate
+                strcat(result, convert);                    // concatenating
+                strcat(result, ":");                        // cancatinating the : with result
+            }
+        }
+
+        // closing the reading end here it will write only
+        close(fdMulresult[0]);
+        write(fdMulresult[1], result, sizeof(result));
 
         // till now the multiplication has been done going for the next step
     }
@@ -109,35 +228,112 @@ int main()
         // parent
         wait(NULL); // waiting for the first p1
 
+         printf("\n parrent \n");
+
+        // will read mul result -- will close writing of that pipe here
+        close(fdMulresult[1]);
+
+        // the result containint
+        char resultMull[500];
+        read(fdMulresult[0], resultMull, sizeof(resultMull));
+
+        char *pipeRead = strtok(resultMull, ":");
+
+        // assigong the first extracted
+        double resultD = strtod(pipeRead, NULL);
+        mullResult[0][0] = resultD;
+
+        // adding the result to the resultant matrix that was produced by the child 1
+        for (int i = 0; i < weightMR && pipeRead != NULL; i++)
+        {
+
+            for (int j = 0; j < featureMC && pipeRead != NULL; j++)
+            {
+                if (!(i == 0 && j == 0))
+                {
+                    // breaking to the pices and writing to the desired array
+                    pipeRead = strtok(NULL, ":");
+                    // assigong the extracted
+                    resultD = strtod(pipeRead, NULL);
+
+                    mullResult[i][j] = resultD;
+                }
+            }
+        }
+
         // making the other process
         prcssId2 = fork();
 
         if (prcssId2 == 0)
         {
+
+            printf("\n child 2 \n");
             // child 2
             // this is p2 will do multithreding here also
             // such that threds equal to number of rows of basis
             // for holding threads ids
             pthread_t threadIds[weightMR];
 
+            int j = 0;
+
             // creating the threads
             for (int i = 0; i < weightMR; i++)
             {
+                j = 0;
+
                 pthread_create(&threadIds[i], NULL, addWithbasis, (void *)&i); // sending row  as the perameter as to add this row
-                pthread_join(threadIds[i], NULL);
+                pthread_join(threadIds[i], (void *)&returnedFromthread);       // waiting for the thread and storing the multiplied result by thread in the resiltant matrix
+
+                // if multiple column output of multiplication
+                while (j < featureMC)
+                {
+
+                    resultantMatrix[i][j] = *(double *)returnedFromthread;
+                    (double *)returnedFromthread++;
+                    j++;
+                }
             }
+
+            // writing this matix output on pipe and will read from that
+            char resultAdd[500];
+            char convertAdd[10];
+
+            for (int i = 0; i < weightMR; i++)
+            {
+                for (int j = 0; j < featureMC; j++)
+                {
+                    sprintf(convertAdd, "%.2f", resultantMatrix[i][j]); // converting to string so to concatenate
+                    strcat(resultAdd, convertAdd);                    // concatenating
+                    strcat(resultAdd, ":");                        // cancatinating the : with result
+                }
+            }
+
+            printf("\n%s\n ",resultAdd);
+
+            // closing reading end here
+            close(fdAddresult[0]);
+            write(fdAddresult[1], resultAdd, sizeof(resultAdd));
+
+
+            printMatrix(resultantMatrix);
         }
 
         // wait till the p2 finishes
         wait(NULL);
+
+         printf("\n parent \n");
 
         // making the other process
         prcssId3 = fork();
 
         if (prcssId3 == 0)
         {
+             printf("\n child 3 \n");
+
             // child
         }
+
+        wait(NULL);
     }
 
     return 0;
@@ -178,9 +374,7 @@ void *addWithbasis(void *args)
     {
 
         result[i] = mullResult[*row][i] + basisMatrix[*row][i];
-
-        // printf("\n%f : %f\n", mullResult[*row][i], basisMatrix[*row][i]);
     }
 
-    return NULL;
+    return (void *)result; // returing the result from thread
 }
